0c79c864fdb6a3cf7b10fc57d63fb2a40bb1c1b2
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-09-30 22:18:09 +0300

    Initial Commit

---
+++ YoutubeKidsClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeKidsClone
+//
+//  Created by Sabyrzhan Tynybayev on 30.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
